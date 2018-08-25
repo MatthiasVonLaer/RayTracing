c1a3648c1574790e45bdf77e4bca5aa43db76ee3
Matthias von Laer <matthias-vonlaer@web.de>
2018-08-26 01:25:27 +0200

    add missing copyright headers

--- test/unit_test/main.cc
+++ test/unit_test/main.cc
@@ -1,3 +1,10 @@
+//  A raytracer program for rendering photorealistic images
+//  Copyright 2018 Matthias von Laer
+//
+//  This program is released under the license GNU GPLv3.
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE UnitTests
 #include <boost/test/unit_test.hpp>
--- tools/checkCopyright.cmd
+++ tools/checkCopyright.cmd
@@ -1,4 +1,11 @@
 #!/bin/bash
+#  A raytracer program for rendering photorealistic images
+#  Copyright 2018 Matthias von Laer
+#
+#  This program is released under the license GNU GPLv3.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 git ls-files \
       | grep -Ev "\.gitignore|\.jpg|\.md" \
       | xargs grep -L "Copyright.*$1.*$2"
