cb356ccef078dbbff3aa43935817347c6983b243
Matthias von Laer <matthias-vonlaer@web.de>
2018-08-26 01:42:10 +0200

    updating license

--- model/shapes/ball.h
+++ model/shapes/ball.h
@@ -1,7 +1,7 @@
 //  A raytracer program for rendering photorealistic images
 //  Copyright 2016, 2018 Matthias von Laer
 //
-//  This program is released under the license GNU GPLv3.
+//  This program is released under the GNU GPLv3.
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
